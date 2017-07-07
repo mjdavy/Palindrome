5adc6cbdd242dce44bc98f18ce3e8adbb6c4b09c
Martin Davy <mjdavy@hotmail.com>
2017-07-07 14:10:26 -0400

    Add project files.

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        /// <summary>
+        /// Find all palindromes of combinations of words in a list
+        /// </summary>
+        /// <param name="words">The word list</param>
+        /// <returns>A list of palindromes</returns>
+        public static IEnumerable<string> FindPalindromes(IEnumerable<string> words)
+        {
+            IEnumerable<string> results = new List<string>();
+
+            var count = words.Count();
+            while (count > 0)
+            {
+                var perms = Perms(words, count--)
+                    .Select(x => x.Aggregate((w, n) => w + n)).Where(x => IsPalindrome(x));
+                results = results.Concat(perms);
+            }
+               
+            return results;
+        }
+
+        /// <summary>
+        /// Recursively find all permutations of a list of words
+        /// </summary>
+        /// <param name="words">The list of words</param>
+        /// <param name="length">The number of words to choose from the list</param>
+        /// <returns>Enumerable containing the permutations</returns>
+        private static IEnumerable<IEnumerable<string>> Perms(IEnumerable<string> words, int length)
+        {
+            if (length == 1)
+            {
+                return words.Select(w => new string[] { w });
+            }
+
+            return Perms(words, length - 1)
+                .SelectMany(w => words
+                .Where(e => !w.Contains(e)), (t1, t2) => t1.Concat(new string[] { t2 }));
+        }
+
+        /// <summary>
+        /// Determines if a word is a palindrome. Case insensitive
+        /// </summary>
+        /// <param name="word">The word to test</param>
+        /// <returns>true is the word is a palindrome, false otherwise</returns>
+        private static bool IsPalindrome(string word)
+        {
+            var reversed = new string(word.Reverse().ToArray());
+            return word.Equals(reversed,StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Five Dwarves ( Gimli Fili Ilif Ilmig and Mark) met at the Prancing Pony and played 
+ * a word game to determine which combinations of their names resulted in a palindrome.
+ * Write a program in that prints out all of those combinations.
+ */
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            IEnumerable<string> palindromes;
+
+            var timer = Stopwatch.StartNew();
+
+            if (args.Count() >= 2)
+            {
+                palindromes = Palindrome.FindPalindromes(args.ToList());
+            }
+            else
+            {
+                palindromes = Palindrome.FindPalindromes(GetNames());
+            }
+
+            timer.Stop();
+
+            // Output
+            Console.WriteLine("Found {0} palindromes in {1} milliseconds", palindromes.Count(), timer.ElapsedMilliseconds);
+            WritePalindromes(palindromes);
+            Console.ReadLine();
+        }
+
+        private static void WritePalindromes(IEnumerable<string> palindromes)
+        {
+            foreach(var palindrome in palindromes)
+            {
+                Console.WriteLine(palindrome);
+            }   
+        }
+
+        static IList<string> GetNames()
+        {
+            var names = new List<string>
+            {
+                "Gimli", "Fili", "Ilif", "Ilmig", "Mark"
+            };
+
+            return names;
+        }
+    }
+}
